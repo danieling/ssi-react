1bbe495985853a2839450c976fe43ae298762904
Edson Terceros <edson.terceros@avantica.net>
2019-01-29 19:59:59 -0400

    postcomment p[arcial

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -14,6 +14,45 @@ export const addComment = (itemId, rating, author, comment) => (
     }
 );
 
+export const postComment = (itemId, rating, author, comment) => (dispatch) => {
+
+    const newComment = {
+        itemId: itemId,
+        rating: rating,
+        author: author,
+        comment: comment
+    };
+    newComment.date = new Date().toISOString();
+
+    return fetch(baseUrl + 'comments',
+        {
+            method: "POST",
+            body: JSON.stringify(newComment),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "same-origin"
+        })
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                throw error;
+            })
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('post comments', error.message);
+            alert('Your comment could not be posted\nError: ' + error.message);
+        });
+};
+
 // es un thunk
 // aqui va la logica de negocio para modificar el state
 // el thunk puede despachar varias acciones
